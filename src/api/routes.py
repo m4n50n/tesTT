4fd88f19338c7322e9dc463d988275eb232e7302
dudubalack <dominguesdatrindadecarloseduar@gmail.com>
2022-08-31 19:30:20 +0000

    arruste en models

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Rol 
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
 
@@ -18,8 +18,8 @@ def login():
 
     print(email)
     if email and password:
-        if User.query.filter_by(email=email).first()==None:
-            user = User(email=email, password=password)
+        if User.query.filter_by(email=email, password=password).first():
+            return jsonify({"msg":"usuario correcto", "loged":True})
         else:
             return jsonify({"msg":"usuario incorrecto", "loged":False})
     else:
