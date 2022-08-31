fcf6e8d13db9f5464feb369daecefa95a2f0477e
carmencami <carmengreta3@outlook.es>
2022-08-31 17:18:07 +0000

    register

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,42 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/register', methods=['POST'])
+def register():
+    email = request.json.get("email")
+    password = request.json.get("password")
+    adresses = request.json.get("adresses")
+    phone = request.json.get("phone")
+    name = request.json.get("name")
+    rol = request.json.get("rol")
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    
+    print(email)
+    if email and password and adresses and phone and name and rol:
+        if User.query.filter_by(email=email).first()==None:
+            user = User(email=email, password=password, adresses=adresses, phone=phone, name=name, rol=rol)
+            db.session.add(user)
+            db.session.commit()
+            token = create_access_token(identity=user.id)
+            return jsonify({"user":user.serialize(), "loged":True, "token":token})
+        else:
+            return jsonify({"msg":"este usuario ya existe", "loged":False})
+    else:
+        return jsonify({"msg":"usuario no creado, revisa la información", "loged":False})
 
-    return jsonify(response_body), 200
+
+
+# @api.route('/private', methods=['GET'])
+# @jwt_required()
+# def private():
+#     user_id=get_jwt_identity()
+#     user=User.query.get(user_id)
+#     if not user:
+#         return jsonify({"msg":"inicie sesión", "loged":False})
+#     else:
+#         return jsonify({"loged":True, "user":user.serialize()})
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,7 +1,40 @@
 const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {},
-    actions: {},
+    actions: {
+      register: (email, password, name, phone, adresses, rol) => {
+        const store = getStore();
+
+        fetch(process.env.BACKEND_URL + "/api/register", {
+          method: "POST",
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            name: name,
+            phone: phone,
+            adreses: adresses,
+            rol: rol,
+          }),
+          headers: {
+            "Content-type": "application/json",
+          },
+        })
+          .then((resp) => {
+            if (resp.ok) {
+              return resp.json();
+            }
+          })
+          .then((data) => {
+            localStorage.setItem("token", data.token);
+            localStorage.setItem("rol", data.rol);
+            setStore({ isAuthenticate: data.loged });
+          })
+          .catch((error) => {
+            setStore({ isAuthenticate: data.loged, msg: data.msg });
+            console.error("[ERROR IN LOGIN]", error);
+          });
+      },
+    },
   };
 };
 
