97a6f5dcf20bcc4164080ed16d41d5de3d5e4594
carmencami <carmengreta3@outlook.es>
2022-10-14 18:08:10 +0000

    navbarresponsive

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -11,28 +11,39 @@ export const Navbar = () => {
   const { store, actions } = useContext(Context);
   const navigate = useNavigate();
   return (
-    <div className="navbar-brand colornavbar">
-      <div className="row menu align-items-center">
-        <div className="col-2">
-          <div className="m-l-20 d-flex">
-            <Link to="/">
-              <img
-                className="m-r-10 logofoto"
-                style={{ width: "120px" }}
-                src={Logoperros}
-                alt="logo"
-              />
-            </Link>
+    <nav className="navbar navbar-expand-lg colornavbar">
+      <div className="container-fluid ">
+        <div className="navbar-brand">
+          <Link to="/">
             <img
-              className="textologo"
-              style={{ width: "120px" }}
-              src={Texto}
-              alt="texto"
+              className="m-r-10 logofoto"
+              style={{ width: "100px" }}
+              src={Logoperros}
+              alt="logo"
             />
-          </div>
+          </Link>
         </div>
-        {!store.isAuthenticate ? (
-          <div className="col-10 ">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler">
+            <i class="fas fa-bars"></i>
+          </span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <img
+            className="textologo"
+            style={{ width: "120px" }}
+            src={Texto}
+            alt="texto"
+          />
+          {!store.isAuthenticate ? (
             <div className="d-flex justify-content-evenly">
               <div className="align-items-start">
                 <Link to="/">
@@ -42,14 +53,14 @@ export const Navbar = () => {
                   </button>
                 </Link>
               </div>
-              <div className="align-items-center">
+              <div className="nav-item">
                 <Link to="/register">
                   <button type="button" className="btn buttonhome">
                     REGISTRATE{" "}
                   </button>
                 </Link>
               </div>
-              <div className="align-items-center">
+              <div className="nav-item">
                 <Link to="/login">
                   <button type="button" className="btn buttonhome">
                     LOGIN{" "}
@@ -57,9 +68,9 @@ export const Navbar = () => {
                 </Link>
               </div>
               <div>
-                <a className="link-dark text-decoration-none" href=""></a>
+                <a className=" text-decoration-none" href=""></a>
               </div>
-              <div className="align-items-end">
+              <div className="nav-item">
                 {/* <Link to="/maps">
                 {" "}
                 <button
@@ -72,46 +83,46 @@ export const Navbar = () => {
               </Link> */}
               </div>{" "}
             </div>
-          </div>
-        ) : (
-          <div className="col-10">
-            <div className="d-flex justify-content-evenly">
-              <div className="align-items-start">
-                <Link to="/">
-                  {" "}
+          ) : (
+            <div>
+              <div className="d-flex justify-content-evenly">
+                <div className="nav-item">
+                  <Link to="/">
+                    {" "}
+                    <button type="button" className="btn buttonhome">
+                      INICIO{" "}
+                    </button>
+                  </Link>
+                </div>
+
+                <Link to="/editusuario">
                   <button type="button" className="btn buttonhome">
-                    INICIO{" "}
+                    PERFIL{" "}
                   </button>
                 </Link>
-              </div>
 
-              <Link to="/editusuario">
-                <button type="button" className="btn buttonhome">
-                  Perfil{" "}
-                </button>
-              </Link>
+                <div className="nav-item">
+                  {" "}
+                  <button
+                    onClick={() => {
+                      actions.logout();
+                      navigate("/");
+                    }}
+                    type="button"
+                    className="btn buttonhome"
+                  >
+                    CERRAR SESIÓN{" "}
+                  </button>
+                </div>
 
-              <div className="align-items-start">
-                {" "}
-                <button
-                  onClick={() => {
-                    actions.logout();
-                    navigate("/");
-                  }}
-                  type="button"
-                  className="btn buttonhome"
-                >
-                  Cerrar sesión{" "}
-                </button>
-              </div>
-
-              <div>
-                <a className="link-dark text-decoration-none" href=""></a>
+                <div>
+                  <a className="link-dark text-decoration-none" href=""></a>
+                </div>
               </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
