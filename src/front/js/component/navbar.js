07453387128c092970701c2221513c2a1ff753c2
carmencami <carmengreta3@outlook.es>
2022-10-17 18:19:12 +0000

    navbarfinish

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -21,7 +21,7 @@ export const Navbar = () => {
           </Link>
         </div>
         <button
-          className="navbar-toggler row"
+          className="navbar-toggler "
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -37,23 +37,20 @@ export const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             {!store.isAuthenticate ? (
-              <div className="d-flex justify-content-end">
-                <li className="nav-item">
-                  <Link to="/">
-                    {" "}
-                    <button type="button" className="btn buttonhome">
-                      INICIO{" "}
-                    </button>
+              <>
+                <li className="nav-item text-end">
+                  <Link type="button" className="btn  buttonhome" to="/">
+                    INICIO
                   </Link>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item text-end">
                   <Link to="/register">
                     <button type="button" className="btn buttonhome">
                       REGISTRATE{" "}
                     </button>
                   </Link>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item text-end">
                   <Link to="/login">
                     <button type="button" className="btn buttonhome">
                       LOGIN{" "}
@@ -63,7 +60,7 @@ export const Navbar = () => {
                 <div>
                   <a className=" text-decoration-none" href=""></a>
                 </div>
-                <div className="nav-item">
+                {/* <div className="nav-item">
                   {/* <Link to="/maps">
                 {" "}
                 <button
@@ -73,13 +70,13 @@ export const Navbar = () => {
                 >
                   MAPS{" "}
                 </button>
-              </Link> */}
-                </div>{" "}
-              </div>
+              </Link> 
+                </div>*/}
+              </>
             ) : (
-              <div>
+              <>
                 <div className="d-block justify-content-evenly">
-                  <li className="nav-item">
+                  <li className="nav-item text-end">
                     <Link to="/">
                       {" "}
                       <button type="button" className="btn buttonhome">
@@ -87,14 +84,14 @@ export const Navbar = () => {
                       </button>
                     </Link>
                   </li>
-
-                  <Link to="/editusuario">
-                    <button type="button" className="btn buttonhome">
-                      PERFIL{" "}
-                    </button>
-                  </Link>
-
-                  <li className="nav-item">
+                  <li className="nav-item text-end">
+                    <Link to="/editusuario">
+                      <button type="button" className="btn buttonhome">
+                        PERFIL{" "}
+                      </button>
+                    </Link>
+                  </li>
+                  <li className="nav-item text-end">
                     {" "}
                     <button
                       onClick={() => {
@@ -115,7 +112,7 @@ export const Navbar = () => {
                     ></a>
                   </div>
                 </div>
-              </div>
+              </>
             )}
           </ul>
         </div>
