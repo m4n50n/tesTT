01bc6df61887670fcdc3101f2fb81d595d7d323f
Jose Clemente García Rodríguez <92524023+m4n50n@users.noreply.github.com>
2022-11-15 19:38:01 +0100

    Update paginalogin.js

--- src/front/js/pages/paginalogin.js
+++ src/front/js/pages/paginalogin.js
@@ -1,107 +1,105 @@
 import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { validate } from "schema-utils";
 import { Context } from "../store/appContext";
+import "../../styles/login.css";
 
 export const Login = () => {
   const { store, actions } = useContext(Context);
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [erorrMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const validate = (email, password) => {
-    setErrorMessage ("")
-    if (!email.includes("@")) {
-      setErrorMessage("email is not correct");
+  const navigate = useNavigate();
+
+  const buttonSubmit = () => {
+    validate(email, password);
+    actions.login(email, password, navigate);
+    if (localStorage.getItem("rol") == 1) {
+      navigate("/protectoralogin");
+    } else if (localStorage.getItem("rol") == 2) {
+      navigate("/casaacogida");
     }
   };
-
+  const validate = (email, password) => {
+    setErrorMessage("");
+    if (!email.includes("@")) {
+      setErrorMessage("El email no es correcto");
+    }
+  };
   return (
-    <login className="footer mt-auto py-3 text-center">
-      <form
-        onSubmit={(ev) => {
-          ev.preventDefault();
-          actions.login(email, password);
-        }}
-      >
-        <div class="form-outline mb-4">
-          <input
-            type="email"
-            id="form2Example1"
-            class="form-control, container-xs"
-            placeholder="email"
-            autoComplete="off"
-            value={email}
-            onChange={(ev) => setEmail(ev.target.value)}
-          ></input>
-          <div>
-            <label class="form-label" for="form2Example1">
-              Correo electronico
-            </label>
-          </div>
-        </div>
-        <div class="form-outline mb-4">
-          <input
-            type="password"
-            id="form2Example2"
-            class="form-control, container-xs"
-            autoComplete="off"
-            value={password}
-            onChange={(ev) => setPassword(ev.target.value)}
-          ></input>
-          <div>
-            <label class="form-label" for="form2Example2">
-              Contraseña
-            </label>
-          </div>
-        </div>
-        <p className="text-danger">{erorrMessage}</p>
-        <div class="row mb-4">
-          <div class="col d-flex justify-content-center">
-            <div class="form-check">
+    <>
+      <div className="wrapper">
+        <div className="card">
+          <div
+            onSubmit={buttonSubmit}
+            action="#"
+            className="d-flex flex-column"
+          >
+            <div className="h3 text-center mb-4 colorlogin">Login</div>
+            <div className="form-group d-flex align-items-center mb-4">
+              <span className="far fa-user p-2 iconologin"></span>
               <input
-                class="form-check-input"
-                type="checkbox"
-                value=""
-                id="form2Example31"
-                checked
+                type="email"
+                id="form2Example1"
+                className="form-control"
+                placeholder="email"
+                autoComplete="off"
+                value={email}
+                onChange={(ev) => setEmail(ev.target.value)}
               />
-           
-              <label class="form-check-label" for="form2Example31">
+            </div>
+            <div className="form-group d-flex align-items-center mb-4">
+              <span className="fas fa-lock p-2 iconologin"></span>
+              <input
+                type="password"
+                id="form2Example2"
+                className="form-control"
+                placeholder="contraseña"
+                autoComplete="off"
+                value={password}
+                onChange={(ev) => setPassword(ev.target.value)}
+              />
+              <div></div>
+            </div>
+            <p className="text-danger">{errorMessage}</p>
+            <div className="row mb-2">
+              <Link className="colortexto" to="/recuperacioncontrasena">
                 {" "}
-                {" "}
-              </label>
+                ¿Olvidó su contraseña?
+              </Link>
             </div>
           </div>
-          <div class="col">
-            <a href="https://3000-carmencami-proyectofina-2bzczlwqvuu.ws-eu64.gitpod.io/recuperacioncontrase%C3%B1a/">Has olvidado tu contraseña?</a>
-          </div>
         </div>
-        <button
-          type="button"
-          onClick={() => {
-            validate(email, password);
-            actions.register(email, password); 
-          }}
-          class="btn btn-primary btn-block mb-4"
-        >
-          Sign in
-        </button>
-        <div class="text-center">
+        <div className="d-flex justify-content-center">
+          <button
+            type="button"
+            onClick={() => {
+              validate(email, password);
+              actions.login(email, password);
+
+              buttonSubmit();
+            }}
+            className="btn botonlogin"
+          >
+            Acceder
+          </button>
+        </div>
+        <div className="text-center">
           <p>
-          ¿Aun no te has registrado? <a href="#!">Registrate</a>
+            ¿Aún no te has registrado? <Link to="/register">Regístrate</Link>
           </p>
-          <p></p>
-          <button type="button" class="btn btn-link btn-floating mx-1">
-            <i class="fab fa-facebook-f"></i>
+          <button type="button" className="btn btn-link btn-floating mx-1">
+            <i className="fab fa-facebook-f"></i>
           </button>
-          <button type="button" class="btn btn-link btn-floating mx-1">
-            <i class="fab fa-instagram"></i>
+          <button type="button" className="btn btn-link btn-floating mx-1">
+            <i className="fab fa-instagram"></i>
           </button>
-          <button type="button" class="btn btn-link btn-floating mx-1">
-            <i class="fab fa-twitter"></i>
+          <button type="button" className="btn btn-link btn-floating mx-1">
+            <i className="fab fa-twitter"></i>
           </button>
         </div>
-      </form>
-    </login>
+      </div>
+    </>
   );
 };
